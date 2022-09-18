8e1a3a2b6e24ca1616056bb6778784c65789bc1b
Ronald Evers <ronald@ch10.nl>
2022-09-18 14:41:47 +0200

    prettier

--- .eleventy.js
+++ .eleventy.js
@@ -1,26 +1,25 @@
 const CleanCSS = require("clean-css");
 const { DateTime } = require("luxon");
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(require("@11ty/eleventy-plugin-syntaxhighlight"));
 
-  eleventyConfig.addFilter("formatDate", function(date) {
+  eleventyConfig.addFilter("formatDate", function (date) {
     const dt = DateTime.fromJSDate(date);
     return `<time datetime="${dt.toISODate()}">${dt.toLocaleString(DateTime.DATE_MED)}</time>`;
   });
 
-  eleventyConfig.addFilter("ronaldify", function(text) {
-    return text.endsWith('Ronald Evers') ? text : `${text} | Ronald Evers`;
+  eleventyConfig.addFilter("ronaldify", function (text) {
+    return text.endsWith("Ronald Evers") ? text : `${text} | Ronald Evers`;
   });
 
-  eleventyConfig.addFilter("cssmin", function(code) {
+  eleventyConfig.addFilter("cssmin", function (code) {
     return new CleanCSS({}).minify(code).styles;
   });
 
   return {
     dir: {
-      input: 'src',
-    }
+      input: "src",
+    },
   };
-
 };
